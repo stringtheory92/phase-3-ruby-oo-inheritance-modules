e5fb3924f12e6575a3e8f9548ab1fe4b6a29816d
Adam Kabak <adamkabak@yahoo.com>
2022-10-31 14:32:14 -0400

    commit tree aaa8474d3fc02d2dd6424cb66f818aef749d885b

---
+++ lib/dance_module.rb
@@ -0,0 +1,19 @@
+module Dance
+    def twirl 
+        "I'm twirling!"
+    end
+
+    def jump 
+        "Look how high I'm jumping!"
+    end
+
+    def pirouette 
+        "I'm doing a pirouette"
+    end
+
+    def take_a_bow 
+        "Thank you, thank you!"
+    end
+
+end
+
